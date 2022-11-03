1034d26097ab2ff4c5c3f523248e305c0e435d4d
aalawneh220 <ahmadalawneh329@gmail.com>
2022-11-03 16:48:30 +0200

    add

---
+++ tourism/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+
+function App() {
+  return (
+    <div >
+      <Nav />
+      <Home />
+      <Footer />
+   
+    </div>
+  );
+}
+
+export default App;
